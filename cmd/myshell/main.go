d06cbf08c54da60274751a1cc9bad1d053229f77
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2024-12-25 16:39:03 +0100

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	commandWithArgs, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	commandWithArgs = commandWithArgs[:len(commandWithArgs) - 1]
-	if err != nil {
-		fmt.Printf("Failed to read input: %s", err.Error())
-		os.Exit(1)
+		// Wait for user input
+		commandWithArgs, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		commandWithArgs = commandWithArgs[:len(commandWithArgs)-1]
+		if err != nil {
+			fmt.Printf("Failed to read input: %s", err.Error())
+			os.Exit(1)
+		}
+
+		command := strings.Split(commandWithArgs, " ")[0]
+		fmt.Printf("%s: command not found\n", command)
 	}
-
-	command := strings.Split(commandWithArgs, " ")[0]
-	fmt.Printf("%s: command not found", command)
 }
